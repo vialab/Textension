a6b14ca5702b87e1cd79bb111fd34ca0a69d351d
jayrsawal <jayrsawal@gmail.com>
2018-07-11 17:16:15 -0400

    Deskewing and OCR optimitimization

--- static/py/InlineViz.py
+++ static/py/InlineViz.py
@@ -9,15 +9,18 @@ import random
 import glob
 import numpy as np
 import cv2
+import imutils
 import base64
 import io
 import googleMaps as gm
 import getngrams as ng
 import matplotlib as mpl
+import math
 mpl.use("Agg")
 import matplotlib.pyplot as plt
 from opacityConversion import *
 from math import ceil, floor
+from skimage.filters import threshold_local
 
 class InlineViz:
     """ Inline Viz wrapper object that performs all algorithm functions and 
@@ -30,7 +33,12 @@ class InlineViz:
     def __init__(self, stream, _translate=False, _max_size=(1024,1024), _pixel_cut_width=5, _noise_threshold=25, _vertical_spread=5, _horizontal_spread=5, _line_buffer=1, _hi_res=True, _rgb_text=(0,0,0), _rgb_bg=(255,255,255), _anti_alias=True, _map_height=150, _blur=0, _google_key=""):
         """ Initialize a file to work with """
         self.max_size = _max_size # maximum size of image for resizing
-        self.img_file = Image.open(stream) # image itself
+        self.img_file = Image.open(stream).convert("RGB") # image itself
+        self.img_file = self.deskewImage(self.img_file)
+        # original = [float(d) for d in self.img_file.size]
+        # scale = 300/72.0
+        # if scale != 1:
+        #     self.img_file.thumbnail([round(scale * d) for d in original], Image.ANTIALIAS)
         self.img_file.thumbnail(self.max_size, Image.ANTIALIAS) # resize image to maximum size
         self.img_width, self.img_height = self.img_file.convert("RGBA").size # resized width and height
         self.line_list = self.detectLines(self.img_file) # X coordinates for vertical lines
@@ -822,6 +830,7 @@ class InlineViz:
 
     def binarizeSharpenImage(self, image):
         """ Prepare image for OCR by converting to grayscale, sharpening, and then binarizing """
+        image = self.increaseContrast(image)
         if not self.binarize:
             return image.convert("RGB")
 
@@ -851,3 +860,126 @@ class InlineViz:
             return img_pil
         else:
             return image
+
+    def increaseContrast(self, image):
+        """ Increase the contrast of an image to improve OCR results """
+        # convert to lab color model
+        img_array = np.asarray(image)
+        lab = cv2.cvtColor(img_array, cv2.COLOR_RGB2LAB)
+        cv2.imshow("lab",lab)
+
+        # split to separate color channels
+        l, a, b = cv2.split(lab)
+
+        # apply CLAHE to L channel
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+        cl = clahe.apply(l)
+
+        # merge enhanced L channel back to A B channels
+        limg = cv2.merge((cl,a,b))
+
+        # return to RGB channel
+        img_cv = cv2.cvtColor(limg, cv2.COLOR_LAB2RGB )
+        img_pil = Image.fromarray(img_cv)
+        return img_pil
+
+    def deskewImage(self, img):
+        """ Ensure that the image is aligned through deskewing """
+        # convert the image to grayscale, blur it, and find edges
+        # in the image
+        image = np.asarray(img)
+        orig = image.copy()
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        edged = cv2.Canny(gray, 75, 200)
+        
+        # find the contours in the edged image, keeping only the
+        # largest ones, and initialize the screen contour
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+        screenCnt = None
+        # loop over the contours
+        for c in cnts:
+            # approximate the contour
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        
+            # if our approximated contour has four points, then we
+            # can assume that we have found our screen
+            if len(approx) == 4:
+                screenCnt = approx
+                break
+        # we could not find four points to deskew
+        if screenCnt is None:
+            return img
+
+        # show the contour (outline) of the piece of paper
+        cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+        # apply the four point transform to obtain a top-down
+        # view of the original image
+        warped = self.four_point_transform(orig, screenCnt.reshape(4, 2))
+        img_pil = Image.fromarray(warped)
+        return img_pil
+
+
+    def four_point_transform(self, image, pts):
+        # obtain a consistent order of the points and unpack them
+        # individually
+        rect = self.order_points(pts)
+        (tl, tr, br, bl) = rect
+    
+        # compute the width of the new image, which will be the
+        # maximum distance between bottom-right and bottom-left
+        # x-coordiates or the top-right and top-left x-coordinates
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+    
+        # compute the height of the new image, which will be the
+        # maximum distance between the top-right and bottom-right
+        # y-coordinates or the top-left and bottom-left y-coordinates
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+    
+        # now that we have the dimensions of the new image, construct
+        # the set of destination points to obtain a "birds eye view",
+        # (i.e. top-down view) of the image, again specifying points
+        # in the top-left, top-right, bottom-right, and bottom-left
+        # order
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+    
+        # compute the perspective transform matrix and then apply it
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    
+        # return the warped image
+        return warped
+
+    def order_points(self, pts):
+        # initialzie a list of coordinates that will be ordered
+        # such that the first entry in the list is the top-left,
+        # the second entry is the top-right, the third is the
+        # bottom-right, and the fourth is the bottom-left
+        rect = np.zeros((4, 2), dtype = "float32")
+    
+        # the top-left point will have the smallest sum, whereas
+        # the bottom-right point will have the largest sum
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+    
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+    
+        # return the ordered coordinates
+        return rect
